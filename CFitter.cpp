57ade5db661a9af61ef21b23d921405b9a8e9a28
Artem Bondarenko <artemb@kth.se>
2018-02-28 06:25:24 +0200

    finished original implementation of fitter worker and its helper

--- CFitter.cpp
+++ CFitter.cpp
@@ -115,16 +115,42 @@ std::vector<std::vector<Layer>> getData(const std::string& fName, const std::vec
 //template for return result
 template<typename T>
 using ResList =  std::vector<
-	std::pair<
-		std::string,
-		std::vector<std::map<size_t, T>>
-	>
+		std::vector<std::pair<
+			std::string,
+			std::map<size_t, T>		//vector of segments(size_t indexed maps of 
+			>>
 >;
+
+//fitter helper, hack arround strong typization
+template<typename T, typename U>
+T fitHelper(U& eng,const std::vector<std::pair<std::array<double, 3>, std::vector<double>>>& pts, const std::array<double, 2> &maxPos, bool cont, const T* const prev, double ubase)
+{
+	static_assert(std::is_base_of<FitEngine<double>, U>::value, "Engine should be derived from FitEngine class");
+	bool useCont = cont&&prev!=nullptr;
+	FitEngine<double>& engine{eng};
+	void* result{nullptr};
+	CGaussParams<double> resCVal;
+	if(std::is_same<T, CGaussParams<double>>::value){
+		resCVal = engine.fitVortCoreCGauss(pts, useCont? (reinterpret_cast<const CGaussParams<double>* const>(prev)->pos):maxPos,
+										   useCont? (reinterpret_cast<const CGaussParams<double>* const>(prev)->r): (5.e-9/ubase));
+		
+		result = reinterpret_cast<void*>(&resCVal);
+	}
+	EGaussParams<double> resEVal;
+	if(std::is_same<T, EGaussParams<double>>::value){
+		std::array<double, 2> prAxisBUp{5.e-9/ubase, 5.e-9/ubase};
+		resEVal = engine.fitVortCoreEGauss(pts, useCont? (reinterpret_cast<const EGaussParams<double>* const>(prev)->pos):maxPos,
+										   useCont? (reinterpret_cast<const EGaussParams<double>* const>(prev)->prAxis):prAxisBUp,
+										   useCont? (reinterpret_cast<const EGaussParams<double>* const>(prev)->angle): 0);
+		result = reinterpret_cast<void*>(&resEVal);
+	}
+	return *reinterpret_cast<T*>(result);
+}
 
 //worker class, template params T -- proper return structure, U -- class name of fitter
 template<typename T, typename U>
 ResList<T> worker(const std::vector<std::string>& files, const std::vector<unsigned int>& layers, bool cont, const size_t tcount,
-	const double thresh, const double radius, const double ubase)
+	const double thresh, const double radius, const double ubase, const bool verbose)
 {
 	//check if the function is called for correct 
 	static_assert(std::is_same<T, CGaussParams<double>>::value || std::is_same<T, EGaussParams<double>>::value, "called a worker() for an unsupported fit function class");
@@ -133,35 +159,60 @@ ResList<T> worker(const std::vector<std::string>& files, const std::vector<unsig
 	//get the logger
 	auto conLog = spdlog::get("main");
 	
-	//make map with all the files
-	ResList<T> results{};
-	for (const auto& file : files)
-		results.push_back({ file, {} });
+	//datatypes used
+	//output result
+	struct Result{
+		//flag to set when the result is done
+		bool ready {false};
+		
+		//indexing stuff
+		size_t fileNumb	{ 0 };
+		
+		//result storage
+		T fit_res;
+		
+		//reference to required segment
+		Result* req {nullptr};
+	};
+	
+	//ticket structure
+	struct Ticket {
+		//file index number
+		size_t fileNumb{ 0 };
+		//supposed maximum
+		std::array<double, 3> max{ 0., 0., 0. };
+		//data read from importer
+		std::vector<std::pair<std::array<double, 3>, std::vector<double>>> pts{};
+		//result drop off point
+		Result *dropRes{nullptr};
+	};
+	
+	//and result list
+	std::vector<
+		std::pair<
+			bool,
+			std::vector<
+				std::map<
+					size_t,
+					Result
+				>
+			>
+		>
+	> results;
+	
+	//initialize it for array of 
+	for(size_t i = 0; i < files.size(); i++)
+		results.push_back({false, {}});
 	//and its mutexes
 	std::mutex resultMutex;
 	std::condition_variable resultCV;
 
 	//make queue for importing files
-	std::queue<std::string> importJobs{};
-	std::vector<std::string> importsDone{};
-	for (const auto& file : files)
-		importJobs.push(file);
+	std::queue<std::pair<size_t, std::string>> importJobs{};
+	for (size_t i = 0; i < files.size(); i++)
+		importJobs.push({i, files[i]});
 	//and its mutex
 	std::mutex impJobMutex;
-
-	//ticket structure
-	struct Ticket {
-		//file number, nothing to do with Linking Park songs :p
-		std::string fileName{ "" };
-		//supposed maximum
-		std::array<double, 3> max{ 0.,0.,0. };
-		//segment number
-		size_t segN{ 0 };
-		//layer number
-		size_t layerN{ 0 };
-		//data points
-		std::vector<std::pair<std::array<double, 3>, std::vector<double>>> pts{};
-	};
 
 	//queue for tickets
 	std::vector<Ticket> tickets{};
@@ -171,38 +222,28 @@ ResList<T> worker(const std::vector<std::string>& files, const std::vector<unsig
 
 	//checking if dependency exists
 	auto existDep = [&](const Ticket& t) {
-		std::string prev{ "" };
-		//first find which file is previous
-		for(auto it = files.begin(); it!= files.end(); ++it)
-			if (it == t.fileName)
-			{
-				if (it == files.begin())
-					return true;
-
-				prev = *std::prev(it);
-			}
-		//find result column
-		auto it = results.begin();
-		for (; it != results.end(); ++it)
-			if (it->first == prev)
-				break;
-
-		if (it->second.empty())
+		if(t->fileNumb !=0 && !results[t->fileNumb - 1].first)
 			return false;
-
-		return it->second.find(t.layerN).second;
+		return t->dropRes->required != nullptr && t->dropRes->required->ready;
 	};
+	
+	//imported counter
+	size_t impCount { 0 };
 
 	//definition of worker thread
-	auto workerThread = [&]() {
+	auto workerThread = [&](const size_t threadN) {
 		//attach and initialize a fitter to a thread
 		U fitEngine;
+		//and open a logger for it
+		auto log = spdlog::stdout_color_st("worker#"+std::to_string(threadN));
+		if(verbose)
+			log->set_level(spdlog::level::debug);
 
 		while (true) 
 		{
 			//ticket for current thread
 			Ticket curT;
-			std::string curFile;
+			std::pair<size_t, std::string> curFile;
 
 			//learn the current state and take decision
 			//don't take ownership of import lock before ticket lock because tickets can be accessed seprately
@@ -234,25 +275,101 @@ ResList<T> worker(const std::vector<std::string>& files, const std::vector<unsig
 
 			if (isImporting)
 			{
+				log->debug("decided to import a file #{}, name is: \"{}\"", curFile.first, curFile.second);
 				//import code to be implemented here
 				//tickets to be merged
 				std::queue<Ticket> fileTickets{};
 
 				//data
-				auto rawData = getData(curFile, layers, thresh, radius, ubase);
-				for (size_t seg = 0; seg < rawData.size(); seg++)
-					for (auto& x : rawData[seg])
+				//TODO implement exception handling
+				auto rawData = getData(curFile.second, layers, thresh, radius, ubase);
+				log->debug("Reading #{}: \"{}\" done, {} segments found.", curFile.first, curFile.second, rawData.size());
+				
+				//lock in result mutex
+				resultMutex.lock();
+				//reserve space for results
+				auto& curResult = results[curFile.first];
+				//set read status to true
+				curResult.first = true;
+				//fill the spaces
+				auto& curSegs = curResult.second;
+				size_t sCnt = 0;
+				for (auto& seg: rawData)
+				{
+					curSegs.push_back({});
+					auto& lastSeg = curSegs.back();
+					//reserve places
+					
+					//temporary container for tickets
+					std::queue<Ticket> tmpTickets{};
+					
+					for(auto& layer: seg)
 					{
+						//check if result has 0 points in slice
+						if(layer.pnts.size() == 0)
+						{
+							log->error("No vortex core found in segment {} layer {} of file: \"{}\"",
+								sCnt, layer.n, curFile.second
+							);
+							continue;
+						}
+						
+						//fill out ticket and result
 						Ticket t;
-						t.fileName = curFile;
-						t.segN = seg;
-						t.layerN = x.n;
-						t.max = std::move(x.max);
-						t.pts = std::move(x.pnts);
-
-						//push new tickets into buffer
-						fileTickets.push(std::move(t));
+						t.fileNumb = curFile.first;
+						t.max = layer.max;
+						t.pts = std::move(layer.pnts);
+						//result
+						Result r;
+						r.ready = false;
+						r.fileNumb = t.fileNumb;
+						
+						//push it into result map
+						lastSeg[layer.n] = r;
+						t.dropRes = &lastSeg[layer.n];
+						
+						//if the whole deal is required to be continuous crosslink
+						if(cont)
+						{
+							//first try linking this file to previous(only if next file is read and 
+							//this file isn't first)
+							if(t.fileNumb != 0&&results[curFile.first - 1].first){
+								if(results[curFile.first - 1].second.size() <= sCnt)
+									log->warn("Current segment {} of \"{}\", didn't exist in previous file", sCnt, curFile.second);
+								else{
+									const auto& prevFileMap = results[curFile.first - 1].second[sCnt];
+									auto prev = prevFileMap.find(layer.n);
+									if(prev != prevFileMap.end())
+										lastSeg[layer.n].required = &prevFileMap[layer.n];
+									else
+										log->warn("Current layer {} in segment {} of \"{}\", didn't exist in previous file",
+												  layer.n, sCnt, curFile.second);
+								}
+							}
+							
+							//then try linking next file, if needed
+							if(t.fileNumb != files.size() - 1 && results[curFile.first + 1].first){
+								if(results[curFile.first + 1].second.size() <= sCnt)
+									log->warn("Current segment {} of \"{}\", doesn't exist in next file", sCnt, curFile.second);
+								else{
+									auto& nextFileMap = results[curFile.first + 1].second[sCnt];
+									auto next = nextFileMap.find(layer.n);
+									if(next != nextFileMap.end() && next->required == nullptr)
+										next->required = &lastSeg[layer.n];
+									else
+										log->warn("Current layer {} in segment {} of \"{}\", doesn't exist in next file",
+												  layer.n, sCnt, curFile.second);
+								}
+							}
+						}
+						tmpTickets.push(std::move(t));
 					}
+					sCnt++;
+				}
+				impCount++;
+				if(impCount == files.size() - 1)
+					moreTicketsExpected = false;
+				resultMutex.unlock();
 
 				ticketMutex.lock();
 				while (!fileTickets.empty())
@@ -260,20 +377,13 @@ ResList<T> worker(const std::vector<std::string>& files, const std::vector<unsig
 					tickets.push_back(std::move(fileTickets.front()));
 					fileTickets.pop();
 				}
-
-				//check if there should be any more jobs
-				impJobMutex.lock();
-				importsDone.push_back(curFile);
-				moreTicketsExpected = importsDone.size() != files.size();
-				impJobMutex.unlock();
-
 				ticketCV.notify_all();
 				ticketMutex.unlock();
-				
 			}
 			else if (isFitting)
 			{
 				//fitting code here
+				log->debug("Fitting this round");
 
 				//handling for getting ticket in case it is needed(either there was none or there is continuity condition)
 				//first wait for tickets to appear if there is none
@@ -285,7 +395,7 @@ ResList<T> worker(const std::vector<std::string>& files, const std::vector<unsig
 						ticketCV.wait(ticketLock);
 				}
 				//getting ticket
-				bool gotTicket{ !cont && waitForTicket };
+				bool gotTicket{ !cont && !waitForTicket };
 				while (!gotTicket) {
 					ticketMutex.lock();
 					if (cont)
@@ -303,6 +413,7 @@ ResList<T> worker(const std::vector<std::string>& files, const std::vector<unsig
 						if (!cont || existDep(*it))
 						{
 							curT = *it;
+							tickets.erase(it);
 							gotTicket = true;
 							break;
 						}
@@ -310,6 +421,20 @@ ResList<T> worker(const std::vector<std::string>& files, const std::vector<unsig
 					if (cont)
 						resultMutex.unlock();
 					ticketMutex.unlock();
+					
+					//TODO check!!
+					//if this is cont run wait for something to change
+					if(!gotTicket){
+						ticketMutex.lock();
+						if(!moreTicketsExpected&&tickets.empty())
+						{
+							ticketMutex.unlock();
+							break;
+						}
+						ticketMutex.unlock();
+						std::unique_lock<std::mutex> resLock(resultMutex);
+						resultCV.wait(resLock);
+					}
 				}
 
 				//if no ticket received anyway continue onto next cycle
@@ -317,17 +442,45 @@ ResList<T> worker(const std::vector<std::string>& files, const std::vector<unsig
 					continue;
 
 				//import code after this point
+				
+				//calling custom helper
+				curT.dropRes->fit_res = fitHelper<T, U>(fitEngine, curT.pts, curT.max, cont, curT.dropRes->required->fit_res, ubase);
+				curT.dropRes->ready = true;
 			}
 			else
 			{
+				log->debug("terminating!");
 				break;
 			}
-		}
+		}//main while(true) loop
 	};
+	
+	
+	std::vector<std::thread> workerArray{};
+	for(size_t i = 0; i<tcount; i++)
+		workerArray.push_back(std::thread(workerThread, i));
+	
+	for(auto& thr: workerArray)
+		thr.join();
+	
+	//make map with all the files
+	ResList<T> retArray{};
+	for(size_t i = 0; i < files.size(); i++)
+	{
+		auto& file = results[i].second;
+		
+		retArray.push_back({files[i], {}});
+		auto& retFile = retArray.back().second;
+		for(auto& seg: file)
+		{
+			retFile.push_back({});
+			auto& cSeg = retFile.back();
+			for(auto& layer: seg)
+				cSeg[layer.first] = layer.second.fit_res;
+		}
+	}
 
-
-
-	return results;
+	return retArray;
 }
 
 
@@ -338,7 +491,7 @@ int main(int argc, char* argv[])
 	auto rLog = spdlog::stdout_color_mt("reader");
 
 	//thread count constant for future paralelism
-	unsigned int threadCnt{ 0 };
+	size_t threadCnt{ 0 };
 	//is sequence of files to be treated as continuous?
 	bool isCont{ false };
 	//if log needs to be verbous?
@@ -368,7 +521,7 @@ int main(int argc, char* argv[])
 		("threads,t", boost::program_options::value<unsigned int>(&threadCnt)->default_value(min(4, std::thread::hardware_concurrency())))
 #else
 		//'u' in the literal is required because of how std::min is defined
-		("threads", boost::program_options::value<unsigned int>(&threadCnt)->default_value(std::min(4u, std::thread::hardware_concurrency())))
+		("threads", boost::program_options::value<size_t>(&threadCnt)->default_value(std::min(4u, std::thread::hardware_concurrency())))
 #endif
 		("output,o", boost::program_options::value<std::string>()->default_value(""), "output text file name, by default table is spit into stdout")
 		("continuous,c", boost::program_options::bool_switch(&isCont)->default_value(false), "treat files as continuous series to improve fit speed")
@@ -399,6 +552,14 @@ int main(int argc, char* argv[])
 		//if help is required do it and quit
 		std::cout << desc << '\n';
 		return 0;
+	}
+	
+	//validate layer list
+	std::sort(layers.begin(), layers.end());
+	if(std::unique(layers.begin(), layers.end()) != layers.end())
+	{
+		conLog->error("layer list contains duplicates!");
+		return -1;
 	}
 
 	//TODO: optional exception handling here 
@@ -462,6 +623,7 @@ int main(int argc, char* argv[])
 
 	conLog->debug("Launch parameters parsed:");
 	//TODO implement exploding wildcards for windows
+	//has to be done with win api, so it can SUCC
 	for(const auto& file: fileNames)
 		if (!boost::filesystem::exists(file) || !boost::filesystem::is_regular_file(file))
 		{
